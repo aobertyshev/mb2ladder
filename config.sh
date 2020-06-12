5ab2dabe1384538234865ac09ce551edd7b20ac1
Anatolii Obertyshev <anatoliy.obertyshev@gmail.com>
2020-06-12 12:02:53 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,3 +3,9 @@ echo 'export MBII_LADDER_WEB_APP_TOKEN_SECRET_KEY="<YOUR_SECRET_KEY_HERE>"' >> ~
 echo 'export MBII_LADDER_DISCORD_BOT_API_KEY="<YOUR_DISCORD_API_KEY_HERE>"' >> ~/.bashrc
 source ~/.bashrc
 echo 'You need to restart shell where you start the processes in order to use these new variables'
+echo 'Restoring dependencies...'
+dotnet restore
+cd ./webapp/ClientApp/
+npm install
+cd ../../
+echo 'Dependencies restored. cd to any project folder and dotnet run'
